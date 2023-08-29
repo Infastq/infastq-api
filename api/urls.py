3e62ff56c8fdbecce6503ca89fc1b5b829c3ec4f
ZeevHaydar <nzhifhaidar@gmail.com>
2023-08-29 18:17:13 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('calculate/', views.calculate),
+    path('summary/', views.get_total_uang)
 ]
