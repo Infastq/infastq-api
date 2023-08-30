4a22826bcf27de68468723c39e1bbb6704f7f74a
Ahmad Rizki <amrizjr@gmail.com>
2023-08-31 05:47:17 +0700

    hapus reactapp 2

--- reactapp/src/services/summary.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-// fetching api dari backend localhost:8000/api/summary/
-export const getSummary = async () => {
-  return await axios.get("https://infastq-api-production.up.railway.app/api/summary/");
-}
