eb94133058e7e082eb316e187cde871bcacb6d6d
ZeevHaydar <nzhifhaidar@gmail.com>
2023-08-31 22:50:13 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello),
     path('calculate/', views.calculate),
-    path('summary/', views.get_total_uang)
+    path('summary/', views.get_total_uang),
+    path('reset/', views.clear, name="reset")
 ]
