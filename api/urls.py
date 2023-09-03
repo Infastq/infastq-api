cfacf451f7ca4a847d4e693828066b8d3d17ab16
ZeevHaydar <nzhifhaidar@gmail.com>
2023-09-03 20:02:45 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('calculate/', views.calculate),
     path('summary/', views.get_total_uang),
     path('reset/', views.clear, name="reset"),
-    path('convert_to_rgb/<int:id1>-<int:id2>/', views.convert_image_to_r5g6b5, name="convert_to_rgb")
+    path('convert_to_rgb/<int:id1>-<int:id2>', views.convert_image_to_r5g6b5, name="convert_to_rgb")
 ]
