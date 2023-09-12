a0fad2fbb7e65ee3504f8126509cef36c87ea8bb
ZeevHaydar <nzhifhaidar@gmail.com>
2023-09-12 18:25:33 +0700

    dwaae

--- api/ml_model.py
+++ api/ml_model.py
@@ -10,21 +10,22 @@ def calculate(red, green, blue):
         'red_freq': [red],
         'green_freq': [green],
         'blue_freq': [blue],
-        # 'r-g': [red-green],
-        # 'r-b':[red-blue],
-        # 'g-b':[green-blue],
+        'r-g': [red-green],
+        'r-b':[red-blue],
+        'g-b':[green-blue],
     }
     df = pd.DataFrame(data=data)
     module_dir = os.path.dirname(__file__)
-  # model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
-    model_path = os.path.join(module_dir, 'ml_models', 'xgboost_model.bin')
-    model = xgb.XGBClassifier()
-    model_json = os.path.join(module_dir, 'ml_models', 'xgboost_model.json')
-    model.load_model(model_json)
+    model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
+    # model_path = os.path.join(module_dir, 'ml_models', 'xgboost_model.bin')
+    # model = xgb.XGBClassifier()
+    # model_json = os.path.join(module_dir, 'ml_models', 'xgboost_model.json')
+    # model.load_model(model_json)
 
-    # model = joblib.load(model_path)
+    model = joblib.load(model_path)
     # result = model.predict(df)
     result = model.predict(df)
-    decoded_result = CustomLabelEncoder(label).inverse_transform(result)
+    # decoded_result = CustomLabelEncoder(label).inverse_transform(result)
+    decoded_result = result
 
     return decoded_result[0]
