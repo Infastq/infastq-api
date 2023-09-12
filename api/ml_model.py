4c8cd4d7efdce4ed55d629a6fc910faff005dfaa
ZeevHaydar <nzhifhaidar@gmail.com>
2023-09-12 10:48:11 +0700

    Update ml_model.py

--- api/ml_model.py
+++ api/ml_model.py
@@ -20,9 +20,9 @@ def calculate(red, green, blue):
     # Construct the path to the model.joblib file
     # model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
     model_path = os.path.join(module_dir, 'ml_models', 'xgboost_model.bin')
-    model = xgb.Booster(model_file=model_path)
+    model = xgb.XGBClassifier(model_file=model_path)
     model_json = os.path.join(module_dir, 'ml_models', 'xgboost_model.json')
-    model = xgb.Booster(model_file=model_json)
+    model.load_model(model_json)
 
     # model = joblib.load(model_path)
     # result = model.predict(df)
