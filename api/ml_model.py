f3798900d6168397f9516b5ad739062ec3b91ffe
ZeevHaydar <nzhifhaidar@gmail.com>
2023-09-12 09:12:09 +0700

    Update ml_model.py

--- api/ml_model.py
+++ api/ml_model.py
@@ -14,11 +14,12 @@ def calculate(red, green, blue):
     }
     df = pd.DataFrame(data=data)
     module_dir = os.path.dirname(__file__)
-
+    data_matrix = xgb.DMatrix(data=df)
     # Construct the path to the model.joblib file
     # model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
     model_path = os.path.join(module_dir, 'ml_models', 'xgboost_model.bin')
     model = xgb.Booster(model_file=model_path)
     # model = joblib.load(model_path)
-    result = model.predict(df)
+    # result = model.predict(df)
+    result = model.predict(data_matrix)
     return result[0]
