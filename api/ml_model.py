723996657d181dcd74316596900ca26da3b15829
ZeevHaydar <nzhifhaidar@gmail.com>
2023-09-12 10:52:46 +0700

    Update ml_model.py

--- api/ml_model.py
+++ api/ml_model.py
@@ -16,17 +16,15 @@ def calculate(red, green, blue):
     }
     df = pd.DataFrame(data=data)
     module_dir = os.path.dirname(__file__)
-    data_matrix = xgb.DMatrix(data=df)
-    # Construct the path to the model.joblib file
-    # model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
+  # model_path = os.path.join(module_dir, 'ml_models', 'model.joblib')
     model_path = os.path.join(module_dir, 'ml_models', 'xgboost_model.bin')
-    model = xgb.XGBClassifier(model_file=model_path)
+    model = xgb.XGBClassifier()
     model_json = os.path.join(module_dir, 'ml_models', 'xgboost_model.json')
     model.load_model(model_json)
 
     # model = joblib.load(model_path)
     # result = model.predict(df)
-    result = model.predict(data_matrix)
+    result = model.predict(df)
     decoded_result = CustomLabelEncoder(label).inverse_transform(result[0])
 
     return decoded_result[0]
